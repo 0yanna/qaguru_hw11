5b328f1394fa1208f821c7f8f5beccee865880a8
0yanna <yannaosodoeva@outlook.com>
2022-02-14 23:42:00 +0300

    added test

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleTest {
+    @Test
+     void selenideSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+    @Test
+    void selenideSearchSecondTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("selenide.org"));
+    }
+}
