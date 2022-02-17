1fe8361784c134122a7ba1c81e197cd5641019ef
0yanna <yannaosodoeva@outlook.com>
2022-02-17 20:19:19 +0300

    added telegram

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -2,6 +2,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -9,6 +10,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+@Tag("simple")
 public class SimpleTest {
     @Test
      void selenideSearchTest() {
